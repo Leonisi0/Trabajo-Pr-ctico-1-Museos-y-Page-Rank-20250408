93a31d0bdb4f7ed035de9b3ad0027c883adcaf61
Leonisi0 <martinleoncuadrado@gmail.com>
2025-04-25 20:18:23 -0300

    FIN

--- template_funciones.py
+++ template_funciones.py
@@ -1,61 +1,75 @@
 def construye_adyacencia(D,m): 
-    # Función que construye la matriz de adyacencia del grafo de museos
-    # D matriz de distancias, m cantidad de links por nodo
-    # Retorna la matriz de adyacencia como un numpy.
     D = D.copy()
-    l = [] # Lista para guardar las filas
-    for fila in D: # recorriendo las filas, anexamos vectores lógicos
-        l.append(fila<=fila[np.argsort(fila)[m]] ) # En realidad, elegimos todos los nodos que estén a una distancia menor o igual a la del m-esimo más cercano
-    A = np.asarray(l).astype(int) # Convertimos a entero
-    np.fill_diagonal(A,0) # Borramos diagonal para eliminar autolinks
+    l = [] 
+    for fila in D:
+        l.append(fila<=fila[np.argsort(fila)[m]] ) 
+    A = np.asarray(l).astype(int) 
+    np.fill_diagonal(A,0) 
     return(A)
 
-def calculaLU(matriz):
-    # matriz es una matriz de NxN
-    # Retorna la factorización LU a través de una lista con dos matrices L y U de NxN.
-    # Completar! Have fun
+def calculaLU(A):
+    m=A.shape[0]    
+    n=A.shape[1]
+    Ac = A.copy()
+    
+    if m!=n:
+        print('Matriz no cuadrada')
+        return
+    
+    for iteracion in range(n - 1):
+        for fila in range(iteracion + 1, n):
+            
+            divisor = Ac[fila][iteracion] / Ac[iteracion][iteracion]
+            Ac[fila][iteracion] = divisor  
+            
+            for columna in range(iteracion + 1, n):  
+                Ac[fila][columna] = Ac[fila][columna] - divisor * Ac[iteracion][columna]
+            
+    L = np.tril(Ac,-1) + np.eye(n)
+    U = np.triu(Ac)
+    
+    return L, U
 
-def calcula_matriz_C(A): 
-    # Función para calcular la matriz de trancisiones C
-    # A: Matriz de adyacencia
-    # Retorna la matriz C
-    Kinv = ... # Calcula inversa de la matriz K, que tiene en su diagonal la suma por filas de A
-    C = ... # Calcula C multiplicando Kinv y A
-    return C
+def calcula_matriz_C(A):
+    K_inversa = np.eye(N, k=0)*(1/m)
+    A_m = construye_adyacencia(D, m)
+    A_transpueta = A_m.T
+    
+    return A_transpueta @ K_inversa 
 
     
 def calcula_pagerank(A,alfa):
-    # Función para calcular PageRank usando LU
-    # A: Matriz de adyacencia
-    # d: coeficientes de damping
-    # Retorna: Un vector p con los coeficientes de page rank de cada museo
     C = calcula_matriz_C(A)
-    N = ... # Obtenemos el número de museos N a partir de la estructura de la matriz A
-    M = ...
-    L, U = calculaLU(M) # Calculamos descomposición LU a partir de C y d
-    b = ... # Vector de 1s, multiplicado por el coeficiente correspondiente usando d y N.
+    N = A.shape[0]
+    I = np.eye(N)
+    M = (N/alfa)*(I-(1-a)*C)
+    L, U = calculaLU(M)
+    b = np.ones(N)
     Up = scipy.linalg.solve_triangular(L,b,lower=True) # Primera inversión usando L
     p = scipy.linalg.solve_triangular(U,Up) # Segunda inversión usando U
     return p
 
 def calcula_matriz_C_continua(D): 
-    # Función para calcular la matriz de trancisiones C
-    # A: Matriz de adyacencia
-    # Retorna la matriz C en versión continua
-    D = D.copy()
-    F = 1/D
-    np.fill_diagonal(F,0)
-    Kinv = ... # Calcula inversa de la matriz K, que tiene en su diagonal la suma por filas de F 
-    C = ... # Calcula C multiplicando Kinv y F
-    return C
+    N = D.shape[0]
+    sumatoria_fila = 0
+    F = np.zeros((N,N))
+    K_inv = np.zeros((N,N))
+    
+    for fila in range(N):
+        sumatoria_fila = 0
+        for columna in range(N):
+            if fila!=columna:
+                F[fila][columna] = 1/D[fila][columna]
+                sumatoria_fila += F[fila][columna]
+        for columna in range(N):
+            if fila==columna:
+                K_inv[fila][columna] = 1/sumatoria_fila
+    return F @ K_inv
 
 def calcula_B(C,cantidad_de_visitas):
-    # Recibe la matriz T de transiciones, y calcula la matriz B que representa la relación entre el total de visitas y el número inicial de visitantes
-    # suponiendo que cada visitante realizó cantidad_de_visitas pasos
-    # C: Matirz de transiciones
-    # cantidad_de_visitas: Cantidad de pasos en la red dado por los visitantes. Indicado como r en el enunciado
-    # Retorna:Una matriz B que vincula la cantidad de visitas w con la cantidad de primeras visitas v
-    B = np.eye(C.shape[0])
-    for i in range(cantidad_de_visitas-1):
-        # Sumamos las matrices de transición para cada cantidad de pasos
+    B = np.eye(C.shape[0])  # C^0
+    C_k = np.eye(C.shape[0])
+    for k in range(cantidad_de_visitas-1):
+        C_k = C_k @ C
+        B += C_k
     return B
